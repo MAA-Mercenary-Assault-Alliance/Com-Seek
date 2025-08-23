d3b787e5bddd48389ae9482e50b16b26c9f2d0a3
Jiramate Kedmake <jiramate2548@gmail.com>
2025-08-23 10:00:17 +0700

    Navbar template

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="bg-[#F2F6FC] flex top-0 w-full h-20 p-4 text-black px-42 justify-center">
-    <div>
-      Hey
-    </div>
+  <div class="flex bg-[#F2F6FC] justify-between items-center top-0 w-full h-18 p-4 text-black px-42 text-xl">
+    <ul class="flex space-x-4">
+      <li><a href="#">LOGO</a></li>
+    </ul>
+    <ul class="flex space-x-15">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Profile</a></li>
+      <!-- Grouped links inside one li -->
+      <li class="flex space-x-3 items-center">
+        <a href="#">Sign in</a>
+        <span>|</span>
+        <a href="#">Sign up</a>
+      </li>
+      <li><a href="#">For Company</a></li>
+    </ul>
+
   </div>
 </template>
 
