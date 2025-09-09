4c1abb405150b7eb037648310023ea9da2c2dd4d
Phasin Sae-ngow <bird.biob3@gmail.com>
2025-09-10 00:32:24 +0700

    format all go files with gofmt and fix struct problems in models

--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "os"
-    "log"
+	"log"
+	"os"
 
-    "com-seek/backend/controllers"
-    "com-seek/backend/database"
+	"com-seek/backend/controllers"
+	"com-seek/backend/database"
 
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    godotenv.Load()
+	godotenv.Load()
 
-    db, dbErr := database.InitDB()
-    if dbErr != nil {
-        log.Fatalf("Failed to connect to the database: %v", dbErr)
-    }
+	db, dbErr := database.InitDB()
+	if dbErr != nil {
+		log.Fatalf("Failed to connect to the database: %v", dbErr)
+	}
 
-    router := controllers.NewRouter(db)
+	router := controllers.NewRouter(db)
 
-    routerErr := router.Run(os.Getenv("SERVER_ADDRESS"))
-    if routerErr != nil {
-        log.Fatal("Could not run server")
-    }
+	routerErr := router.Run(os.Getenv("SERVER_ADDRESS"))
+	if routerErr != nil {
+		log.Fatal("Could not run server")
+	}
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -6,11 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	Email			string	`json:"email" gorm:"type:varchar(255);uniqueIndex:idx_email,priority:1,length:191;not null"`
-	Password	string	`json:"password";gorm:"not null"`
+	Email    string `json:"email" gorm:"type:varchar(255);uniqueIndex:idx_email,priority:1,length:191;not null"`
+	Password string `json:"password" gorm:"not null"`
 }
 
 type Admin struct {
-	UserID		uint
-	User			User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:ID"`
+	UserID uint
+	User   User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:ID"`
 }
