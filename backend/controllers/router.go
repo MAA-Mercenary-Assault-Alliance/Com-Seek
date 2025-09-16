b60213ece8fd4834abbdd3bace45aea945b81b8d
Phasin Sae-ngow <bird.biob3@gmail.com>
2025-09-16 22:23:21 +0700

    add simple student profile API to test auth middleware

--- backend/controllers/router.go
+++ backend/controllers/router.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"com-seek/backend/middlewares"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -9,10 +11,16 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 	router := gin.Default()
 
 	authController := NewAuthController(db)
+	StudentController := NewStudentController(db)
 
 	authGroup := router.Group("/auth")
 	authGroup.POST("/register", authController.CreateUser)
 	authGroup.POST("/login", authController.Login)
+
+	requiredLogin := router.Group("/", middlewares.CheckAuth)
+
+	student := requiredLogin.Group("/students")
+	student.GET("/", StudentController.GetStudentProfile)
 
 	return router
 }
