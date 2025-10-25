e6a196513c474395af5f18b03b12d039a25e4fcb
Jiramate Kedmake <jiramate2548@gmail.com>
2025-10-25 12:03:24 +0700

    Add Modal with File Upload

--- frontend/src/components/CVBox.vue
+++ frontend/src/components/CVBox.vue
@@ -24,6 +24,9 @@ onMounted(() => {
   });
 });
 
+const fileInput = ref(null)
+const selectedFile = ref(null)
+
 async function applyJob() {
   if (role.value !== 'student') {
     alert("Only students can apply to jobs.")
@@ -40,6 +43,7 @@ async function applyJob() {
     return
   } catch (error) {
     console.error("Error applying to job:", error.response.data)
+    close()
     if (error.response.data.error == "job application already exists") {
       alert("You have already applied to this job.")
     } else {
@@ -47,13 +51,49 @@ async function applyJob() {
     }
   }
 }
+
+function handleFileUpload(event) {
+  const file = event.target.files[0] // take the first selected file
+  if (file) {
+    selectedFile.value = file
+    console.log('Selected file:', file)
+  }
+}
 </script>
 
 <template>
   <dialog ref="dialog" class="modal">
     <div class="modal-box">
-      <div class="flex flex-col justify-center items-center p-10">
+      <div class="flex flex-col justify-center items-center p-10 space-y-5">
         <p class="py-4 text-xl">Upload your CV/Resume here</p>
+        <div class="relative inline-flex items-center">
+          <!-- Hidden file input -->
+          <input
+              ref="fileInput"
+              type="file"
+              accept=".pdf"
+              @change="handleFileUpload"
+              class="absolute w-0 h-0 opacity-0 overflow-hidden"
+          />
+
+          <!-- Styled button -->
+          <button
+              type="button"
+              @click="fileInput.click()"
+              class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
+          >
+            Upload PDF
+          </button>
+
+          <!-- Display selected file name -->
+          <span v-if="selectedFile" class="ml-2 text-gray-700">{{ selectedFile.name }}</span>
+        </div>
+
+        <button class="btn shadow-none border-0 h-10 w-30 rounded-md text-white text-md font-extralight px-7 disabled:bg-gray-200 bg-lighter"
+                :disabled="!selectedFile"
+                @click="applyJob">
+          Apply
+        </button>
       </div>
 
     </div>
