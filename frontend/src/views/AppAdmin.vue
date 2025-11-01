275201e29e8f11dc780ca836a52fb38d28d03585
Jiramate Kedmake <jiramate2548@gmail.com>
2025-11-01 11:54:57 +0700

    Add leaf svg to make empty message at admin dashboard

--- frontend/src/views/AppAdmin.vue
+++ frontend/src/views/AppAdmin.vue
@@ -66,13 +66,13 @@ async function selectTab(tab: string) {
 </script>
 
 <template>
-  <div class="flex flex-col w-full">
+  <div class="flex flex-grow flex-col w-full">
 
     <div class="flex w-full h-40 px-42 items-center bg-[url('/adminBar.png')] bg-right">
       <span class="text-white text-5xl font-bold">Admin Dashboard</span>
     </div>
 
-    <div id="content" class="flex flex-row space-x-10">
+    <div id="content" class="flex flex-row space-x-10 h-full">
       <div id="sidebar" class="hidden sm:flex flex-col w-1/5">
         <div class="sidebar-properties" :class="[activeTab === 'companies' ? 'sidebar-highlight' : 'sidebar-normal']" @click="selectTab('companies')">
           <span class="ml-4 xl:ml-15 text-xl xl:text-3xl">Company</span>
@@ -84,18 +84,27 @@ async function selectTab(tab: string) {
           <span class="ml-4 xl:ml-15 text-xl xl:text-3xl">Job Posting</span>
         </div>
       </div>
-      <div v-if="!isLoading" id="boxes" class="flex relative flex-col mt-10 space-y-6 w-4/5 xl:pr-42 pb-10">
+      <div v-if="!isLoading" id="boxes" class="flex flex-grow relative flex-col mt-10 space-y-6 w-4/5 xl:pr-42 pb-10">
         <template v-if="activeTab === 'companies'">
-          <CompanyBox v-if="company_list.length > 0" v-for="company in company_list" :key="company.UserID" :company-info="company" @refresh="getCompanies"/>
-          <span v-else class="text-2xl text-gray-500 mt-10 ml-10">No companies pending approval</span>
+          <CompanyBox v-if="company_list" v-for="company in company_list" :key="company.UserID" :company-info="company" @refresh="getCompanies"/>
+          <div v-else class="flex flex-col flex-grow items-center justify-center -translate-y-10">
+            <img src="../../src/assets/leaf.svg" class="w-50" alt="leaf"/>
+            <span class="text-2xl text-gray-500 mt-10">No companies pending approval</span>
+          </div>
         </template>
         <template v-if="activeTab === 'students'">
-          <StudentBox v-if="student_list.length > 0" v-for="student in student_list" :key="student.UserID" :student-info="student" @refresh="getStudents"/>
-          <span v-else class="text-2xl text-gray-500 mt-10 ml-10">No students pending approval</span>
+          <StudentBox v-if="student_list" v-for="student in student_list" :key="student.UserID" :student-info="student" @refresh="getStudents"/>
+          <div v-else class="flex flex-col flex-grow items-center justify-center -translate-y-10">
+            <img src="../../src/assets/leaf.svg" class="w-50" alt="leaf"/>
+            <span class="text-2xl text-gray-500 mt-10">No students pending approval</span>
+          </div>
         </template>
         <template v-if="activeTab === 'jobs'">
-          <JobBoxAdmin v-if="job_list.length > 0" v-for="job in job_list" :key="job.UserID" :job-info="job" @refresh="getJobs"/>
-          <span v-else class="text-2xl text-gray-500 mt-10 ml-10">No jobs pending approval</span>
+          <JobBoxAdmin v-if="job_list" v-for="job in job_list" :key="job.UserID" :job-info="job" @refresh="getJobs"/>
+          <div v-else class="flex flex-col flex-grow items-center justify-center -translate-y-10">
+            <img src="../../src/assets/leaf.svg" class="w-50" alt="leaf"/>
+            <span class="text-2xl text-gray-500 mt-10">No jobs pending approval</span>
+          </div>
         </template>
       </div>
     </div>
