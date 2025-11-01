9ccb6c4016763e3a6dee43eed18781cc746303e8
Jiramate Kedmake <jiramate2548@gmail.com>
2025-11-01 10:43:52 +0700

    Fix accepting job box opening the job page (propagation problem)

--- frontend/src/components/JobBoxAdmin.vue
+++ frontend/src/components/JobBoxAdmin.vue
@@ -80,7 +80,7 @@ onMounted(() => {
       <span class="text-gray-500">{{ date }}</span>
       <img src="../assets/newTab.svg" class="w-5 h-5 cursor-pointer" alt="new-tab-icon" @click.stop="goToJob(true)"/>
       <div class="flex flex-row mt-auto space-x-6">
-        <button class="btn shadow-none bg-[#1F7AB9] border-0 h-8 rounded-4xl text-white text-md font-extralight px-7" @click="acceptJob">Accept</button>
+        <button class="btn shadow-none bg-[#1F7AB9] border-0 h-8 rounded-4xl text-white text-md font-extralight px-7" @click.stop="acceptJob">Accept</button>
         <button class="btn shadow-none bg-[#9A0000] border-0 h-8 rounded-4xl text-white text-md font-extralight px-7" @click="confirmBox.open()">Reject</button>
       </div>
     </div>
