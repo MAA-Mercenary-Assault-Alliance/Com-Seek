0ad65d58a8e8834ba424fdf4f973ea8627e3b472
Phasin Sae-ngow <bird.biob3@gmail.com>
2025-11-04 23:44:07 +0700

    Add deletion of file if registration failed and clean up refactoring artifacts

--- backend/controllers/authController.go
+++ backend/controllers/authController.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"time"
 
+	"com-seek/backend/helpers"
 	"com-seek/backend/models"
 
 	"github.com/gin-gonic/gin"
@@ -60,10 +61,8 @@ func (ac *AuthController) RegisterStudent(c *gin.Context) {
 	}
 
 	var userFound models.User
-	ac.DB.Where("email=?", input.Email).Find(&userFound)
-
-	if userFound.ID != 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "email already registered"})
+	if err := ac.DB.Where("email = ?", input.Email).First(&userFound).Error; err == nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "email already registered"})
 		return
 	}
 
@@ -102,15 +101,17 @@ func (ac *AuthController) RegisterStudent(c *gin.Context) {
 
 	if err := tx.Create(&profile).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create student profile: " + err.Error()})
+		helpers.DeleteFileRecord(ac.DB, transcript.ID)
 		return
 	}
 
 	if err := tx.Commit().Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "transaction commit failed: " + err.Error()})
+		helpers.DeleteFileRecord(ac.DB, transcript.ID)
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "student user created successfully", "user": user})
+	c.JSON(http.StatusOK, gin.H{"message": "student user created successfully"})
 }
 
 func (ac *AuthController) RegisterCompany(c *gin.Context) {
@@ -164,92 +165,8 @@ func (ac *AuthController) RegisterCompany(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "company user created successfully", "user": user})
+	c.JSON(http.StatusOK, gin.H{"message": "company user created successfully"})
 }
-
-// func (ac *AuthController) CreateUser(c *gin.Context) {
-// 	var input RegisterInput
-
-// 	if err := c.ShouldBindJSON(&input); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	var userFound models.User
-// 	ac.DB.Where("email=?", input.Email).Find(&userFound)
-
-// 	if userFound.ID != 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "email already registered"})
-// 		return
-// 	}
-
-// 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to hash password"})
-// 		return
-// 	}
-
-// 	user := models.User{
-// 		Email:    input.Email,
-// 		Password: string(passwordHash),
-// 	}
-
-// 	tx := ac.DB.Begin()
-// 	defer tx.Rollback()
-
-// 	if err := tx.Create(&user).Error; err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	switch input.UserType {
-// 	case "student":
-// 		studentProfile := input.StudentProfile
-// 		if studentProfile == nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": "Student profile is required for student type"})
-// 			return
-// 		}
-
-// 		profile := models.Student{
-// 			UserID:    user.ID,
-// 			FirstName: studentProfile.FirstName,
-// 			LastName:  studentProfile.LastName,
-// 			IsAlum:    *studentProfile.IsAlum,
-// 		}
-
-// 		if err := tx.Create(&profile).Error; err != nil {
-// 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 			return
-// 		}
-
-// 	case "company":
-// 		companyProfile := input.CompanyProfile
-// 		if companyProfile == nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": "Company profile is required for company type"})
-// 			return
-// 		}
-
-// 		profile := models.Company{
-// 			UserID:        user.ID,
-// 			Name:          companyProfile.Name,
-// 			Location:      companyProfile.Location,
-// 			ContactEmail:  companyProfile.ContactEmail,
-// 			ContactNumber: companyProfile.ContactNumber,
-// 		}
-
-// 		if err := tx.Create(&profile).Error; err != nil {
-// 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 			return
-// 		}
-// 	}
-
-// 	if err := tx.Commit().Error; err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{"data": user})
-// }
 
 type LoginInput struct {
 	Email    string `json:"email" binding:"required"`
