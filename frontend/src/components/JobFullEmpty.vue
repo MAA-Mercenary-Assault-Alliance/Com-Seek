5892abc8030be2515c1b6c73a697abfa130e7819
Phasit Ruangmak <phasit2005@gmail.com>
2025-11-17 03:08:04 +0700

    Company Job change

--- frontend/src/components/JobFullEmpty.vue
+++ frontend/src/components/JobFullEmpty.vue
@@ -5,7 +5,7 @@
 <template>
   <div id="job full" class="flex relative w-2/3 px-25 py-10 flex-col rounded-2xl box-shadow bg-white w-full">
 
-  <router-link to="/create-job" class="btn btn-success">
+  <router-link to="/create-job" class="btn bg-[#44B15B] border-[#44B15B] hover:bg-[#56A45C] hover:border-[#56A45C] text-white">
     Create Job
   </router-link>
 
