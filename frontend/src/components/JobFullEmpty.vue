0adff8999004c796c7d08ad556ff9eea9397f0e6
Phasit Ruangmak <phasit2005@gmail.com>
2025-11-17 02:53:48 +0700

    Job Box fix

--- frontend/src/components/JobFullEmpty.vue
+++ frontend/src/components/JobFullEmpty.vue
@@ -3,9 +3,11 @@
 </script>
 
 <template>
-  <div id="job full" class="flex relative w-2/3 px-25 py-10 flex-col rounded-2xl box-shadow bg-white">
+  <div id="job full" class="flex relative w-2/3 px-25 py-10 flex-col rounded-2xl box-shadow bg-white w-full">
 
-    <span>Select a Job</span>
+  <router-link to="/create-job" class="btn btn-success">
+    Create Job
+  </router-link>
 
   </div>
 </template>
