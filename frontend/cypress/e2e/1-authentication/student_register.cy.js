15bcd964728240dadc00edfc838c0565af4dfb61
Jaykayy <jiramate2548@gmaill.com>
2025-11-17 11:44:20 +0700

    Working Student Login test case

--- frontend/cypress/e2e/1-authentication/student_register.cy.js
+++ frontend/cypress/e2e/1-authentication/student_register.cy.js
@@ -5,29 +5,33 @@ context('Actions', () => {
 
   // https://on.cypress.io/interacting-with-elements
 
-  it('Register as a student', () => {
-    cy.contains('Get Started').click()
+  // it('Register as a student', () => {
+  //   cy.contains('Get Started').click()
+  //
+  //   cy.get('#email').type("student@gmail.com")
+  //   cy.get('#reg-password').type("12345678")
+  //   cy.get('#reg-confirm').type("12345678")
+  //   cy.get("#firstName").type("John")
+  //   cy.get("#lastName").type("Doe")
+  //
+  //   cy.get('#register-button').click()
+  //
+  //   cy.get('#terms-scroll-area').scrollTo('bottom')
+  //   cy.get("#agree").click()
+  //   cy.get("#accept-terms").click()
+  // })
 
-    cy.get('#email').type("student@gmail.com")
-    cy.get('#reg-password').type("12345678")
-    cy.get('#reg-confirm').type("12345678")
-    cy.get("#firstName").type("John")
-    cy.get("#lastName").type("Doe")
+  it('Login as a student', function () {
+    cy.fixture("exampleStudent").then((data) => {
+      cy.contains("Log in").click()
 
-    cy.get('#register-button').click()
+      cy.get("#email").type(data.email)
+      cy.get("#login-password").type(data.password)
 
-    cy.get('#terms-scroll-area').scrollTo('bottom')
-    cy.get("#agree").click()
-    cy.get("#accept-terms").click()
-  })
+      cy.contains("LOGIN").click()
 
-  it('Login as a student', () => {
-    cy.contains("Log in").click()
-
-    cy.get("#email").type("student@gmail.com")
-    cy.get("#login-password").type("12345678")
-
-    cy.contains("LOGIN").click()
+      cy.contains("Search for your jobs now").should("be.visible");
+    });
   })
 
 })
